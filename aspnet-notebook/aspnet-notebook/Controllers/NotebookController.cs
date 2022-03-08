265924d4df2536ee62ac2400da8cf15703c59c38
Hargaaya <hargaaya10@gmail.com>
2022-03-08 18:51:26 +0100

    Learned about passing data, Routing, ViewBag & ViewData dictionaries and most importantly about controllers. Next up -> Db

--- aspnet-notebook/aspnet-notebook/Controllers/NotebookController.cs
+++ aspnet-notebook/aspnet-notebook/Controllers/NotebookController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using aspnet_notebook.Models;
 
 namespace aspnet_notebook.Controllers
 {
@@ -6,7 +7,23 @@ namespace aspnet_notebook.Controllers
     {
         public IActionResult Index()
         {
+            ViewData["What"] = "This passes into view O_o";
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult New()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult New(string title, string text)
+        {
+            Console.WriteLine($"the title is: {title}, and the text: {text}");
+
+            NotebookItem newItem = new NotebookItem { Title = title, Text = text };
+            return View("Success", newItem);
         }
     }
 }
--- aspnet-notebook/aspnet-notebook/Models/NotebookItem.cs
+++ aspnet-notebook/aspnet-notebook/Models/NotebookItem.cs
@@ -1,16 +1,15 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace aspnet_notebook.Models
 {
     public class NotebookItem
     {
         public Guid ItemId { get; set; }
-        public bool IsRead { get; set; }
+        public bool IsRead { get; set; } = false;
 
         [Required]
         public string Title { get; set; }
         public string? Text { get; set; }
-        public DateTimeOffset? Created { get; set; }
+        public DateTimeOffset? Created { get; set; } = DateTimeOffset.Now;
     }
 }
