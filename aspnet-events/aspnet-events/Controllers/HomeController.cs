59ef10b1e61535f1172a0aa33dc82acbe2c92921
Hargaaya <hargaaya10@gmail.com>
2022-03-11 12:22:44 +0100

    aspnet-events init

---
+++ aspnet-events/aspnet-events/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet_events.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
