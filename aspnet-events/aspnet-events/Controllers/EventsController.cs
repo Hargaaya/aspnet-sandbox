df718243af460bbb77253cf3bc7c7cc6af375423
Hargaaya <hargaaya10@gmail.com>
2022-03-11 13:17:10 +0100

    EventsController

---
+++ aspnet-events/aspnet-events/Controllers/EventsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet_events.Controllers
+{
+    public class EventsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Join(Guid id)
+        {
+            return View();
+        }
+
+        public IActionResult Confirmation(Guid id)
+        {
+            return View();
+        }
+
+        public IActionResult Booked()
+        {
+            return View();
+        }
+    }
+}
