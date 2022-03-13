cd3f25519e5211e8df014e861b84ff71a36f2868
Hargaaya <hargaaya10@gmail.com>
2022-03-14 00:37:15 +0100

    Updated controller to use the new service class

--- aspnet-events/aspnet-events/Controllers/EventsController.cs
+++ aspnet-events/aspnet-events/Controllers/EventsController.cs
@@ -1,20 +1,31 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using aspnet_events.Services;
 
 namespace aspnet_events.Controllers
 {
     public class EventsController : Controller
     {
+
+        private IEventsService _es;
+
+        public EventsController(IEventsService eventsService)
+        {
+            _es = eventsService;
+        }
+
+
         public IActionResult Index()
         {
+            ViewBag.Events = _es.GetEvents();
             return View();
         }
 
-        public IActionResult Join(Guid id)
+        public IActionResult Join(int id)
         {
             return View();
         }
 
-        public IActionResult Confirmation(Guid id)
+        public IActionResult Confirmation(int id)
         {
             return View();
         }
