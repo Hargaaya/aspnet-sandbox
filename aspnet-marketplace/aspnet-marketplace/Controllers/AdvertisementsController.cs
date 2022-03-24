39724a2f5cb16666d11d8013aa063b7f37f56a8b
Hargaaya <hargaaya10@gmail.com>
2022-03-24 21:02:24 +0100

    Added controller + view for advertisements

---
+++ aspnet-marketplace/aspnet-marketplace/Controllers/AdvertisementsController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace aspnet_marketplace.Controllers
+{
+    public class AdvertisementsController : Controller
+    {
+        [HttpPost]
+        public IActionResult search(string searchParam, int catId)
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult category(int catId)
+        {
+            return View();
+        }
+    }
+}
